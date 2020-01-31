27b1e8a5936f516049c01fb6e05c3b262e28ab04
Zapmon <60135158+Zapmon@users.noreply.github.com>
2020-01-31 17:00:42 +0530

    Add files via upload

---
+++ 31 Jan 2020/DemoProject/DemoProject/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  DemoProject
+//
+//  Created by zapbuild on 30/01/20.
+//  Copyright © 2020 zapbuild. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+       
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
